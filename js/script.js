3970ac676c74f31748de1172c8c7b995dd50f54a
M.Aldi Gunawan <aldigunawan1808@gmail.com>
2024-08-19 20:30:19 +0700

    Create Initial Grid
    grid cells have four sections:
    
    header
    aside
    main
    footer

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log("Hello World!!");
+const searchBox = document.querySelector('.search-box');
+
+searchBox.addEventListener('click', (e) => {
+  const label = e.target.closest('#search-icon');
+
+  if(label) {
+    searchBox.classList.toggle('open');
+  }
+})
